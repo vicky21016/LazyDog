3b56f77a243cc9e9029e3daefa7eea56b655e0b6
Zoe <a033117336@gmail.com>
2025-01-22 14:33:22 +0800

    母檔

---
+++ lazydog/app/product/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+export default function AppPage(props) {
+  return (
+    <>
+      <div>App Page</div>
+    </>
+  );
+}
---
+++ lazydog/app/product/template/js/pd-list.js
@@ -0,0 +1,29 @@
+const ranges = document.querySelectorAll("input[type=range]");
+let max, min;
+let events = ["input", "mouseenter"];
+
+max = document.querySelector(".range-max");
+min = document.querySelector(".range-min");
+let to =
+  (max.value / (max.getAttribute("max") - max.getAttribute("min"))) * 100;
+let from =
+  (min.value / (max.getAttribute("max") - max.getAttribute("min"))) * 100;
+ranges[0].style.setProperty("--to", to);
+ranges[0].style.setProperty("--from", from);
+
+events.forEach((event) => {
+  ranges.forEach((range) => {
+    range.addEventListener(event, function () {
+      // console.log(this.value);
+      max = document.querySelector(".range-max").value;
+      min = document.querySelector(".range-min").value;
+      let to = ((max - this.min) / (this.max - this.min)) * 100;
+      let from = ((min - this.min) / (this.max - this.min)) * 100;
+      // console.log(to);
+      // console.log(from);
+      // 单值选择高亮起始位置一定是0
+      ranges[0].style.setProperty("--to", to);
+      ranges[0].style.setProperty("--from", from);
+    });
+  });
+});
