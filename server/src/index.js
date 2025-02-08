fda45acc89603e17aa1d3aa8bde746aa6c5d2ca1
Zoe <a033117336@gmail.com>
2025-02-09 00:48:53 +0800

    0209 server連結

--- server/src/index.js
+++ server/src/index.js
@@ -1,60 +1,36 @@
-import express from "express";
-import multer from "multer";
 import dotenv from "dotenv";
+import express from "express";
 import cors from "cors";
-import jwt from "jsonwebtoken";
+import authRouter from "./routes/authRoutes.js";
+import connectToDatabase from "./config/mysql.js";
 
 dotenv.config();
 
 const app = express();
-const upload = multer();
-const secretKey = process.env.JWT_SECRET_KEY;
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST"],
+  })
+);
 
-// CORS 設定
-const whitelist = ["http://localhost:5500", "http://localhost:3000"];
-const corsOptions = {
-  credentials: true,
-  origin(origin, callback) {
-    if (!origin || whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.warn(`⚠️ CORS blocked request from: ${origin}`);
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
 
-app.use(cors(corsOptions));
-app.use(express.json()); 
-
-// 測試首頁 API
+app.use("/auth", authRouter); 
 app.get("/", (req, res) => {
-  res.send("首頁");
+  res.json({ status: "success", data: null, message: "首頁" });
 });
 
-// JWT
-function checkToken(req, res, next) {
-  let token = req.get("Authorization");
 
-  if (token && token.startsWith("Bearer ")) {
-    token = token.slice(7);
-    jwt.verify(token, secretKey, (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ status: "error", message: "登入驗證失效，請重新登入。" });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
+(async () => {
+  try {
+    const db = await connectToDatabase();
+    console.log("資料庫已連接");
+
+    app.listen(5000, () => {
+      console.log("服務啟動於 http://localhost:5000");
     });
-  } else {
-    return res.status(401).json({ status: "error", message: "無登入驗證資料，請重新登入。" });
+  } catch (err) {
+    console.error("無法啟動伺服器，資料庫連接失敗:", err);
   }
-}
-
-
-
-// 設置 PORT 並啟動伺服器
-const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+})();
