80cc541b5955fc7cc17d59dd712c0dc8637ace85
Zoe <a033117336@gmail.com>
2025-02-08 15:40:51 +0800

    0208 06 轉modlues css 14-13

--- lazydog/app/hotel-coupon/fonthotelDetail/page.js
+++ lazydog/app/hotel-coupon/fonthotelDetail/page.js
@@ -5,6 +5,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import "../../../html/hotel-coupon/css/fontHotelHome.css";
+import hotelStyles from "../../../styles/modules/fontHotelDetail.module.css";
+import styles from "../../../styles/modules/fontHotelHome.module.css";
 import "nouislider/dist/nouislider.css";
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
@@ -69,60 +71,61 @@ export default function HotelHomePage() {
 
   return (
     <>
-      <div className="container mt-4">
-        <div className="suSearch-bar">
-          <div className="suSearch-group">
-            <img
-              className="suIcon"
-              src="/hotel/hotel-images/page-image/icon-search.png"
-              alt=""
-            />
-            <input
-              type="text"
-              className="suSearch-input"
-              placeholder="搜尋關鍵字、地區..."
-              value={location}
-              readOnly
-              onClick={openModal}
-            />
+        <div className="container mt-4">
+            <div className={styles.suSearchBar}>
+              <div className={styles.suSearchGroup}>
+                <img
+                  className={styles.suIcon}
+                  src="/hotel/hotel-images/page-image/icon-search.png"
+                  alt=""
+                />
+                <input
+                  type="text"
+                  className={styles.suSearchInput}
+                  placeholder="搜尋關鍵字、地區..."
+                  value={location}
+                  readOnly
+                  onClick={openModal}
+                />
+              </div>
+              <div className={styles.suSearchGroup}>
+                <img
+                  className={styles.suIcon}
+                  src="/hotel/hotel-images/page-image/icon-Calendar.png"
+                  alt=""
+                />
+                <input
+                  type="text"
+                  id="date-date"
+                  className={styles.suSearchDate}
+                  placeholder="入住日期 → 退房日期"
+                />
+              </div>
+              <div className={styles.suSearchGroup}>
+                <img
+                  className={styles.suIcon}
+                  src="/hotel/hotel-images/page-image/Icon-mimi.png"
+                  alt=""
+                />
+                <span className="text">數量</span>
+                <button
+                  className={styles.suQuantityBtn}
+                  onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}
+                >
+                  -
+                </button>
+                <span className={styles.suQuantityNumber}>{quantity}</span>
+                <button
+                  className={styles.suQuantityBtn}
+                  onClick={() => setQuantity(quantity + 1)}
+                >
+                  +
+                </button>
+              </div>
+              <button className={styles.suSearchBtn}>搜尋</button>
+            </div>
           </div>
-          <div className="suSearch-group">
-            <img
-              className="suIcon"
-              src="/hotel/hotel-images/page-image/icon-Calendar.png"
-              alt=""
-            />
-            <input
-              type="text"
-              id="date-date"
-              className="suSearch-date"
-              placeholder="入住日期 → 退房日期"
-            />
-          </div>
-          <div className="suSearch-group">
-            <img
-              className="suIcon"
-              src="/hotel/hotel-images/page-image/Icon-mimi.png"
-              alt=""
-            />
-            <span className="text">數量</span>
-            <button
-              className="suQuantity-btn"
-              onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}
-            >
-              -
-            </button>
-            <span>{quantity}</span>
-            <button
-              className="suQuantity-btn"
-              onClick={() => setQuantity(quantity + 1)}
-            >
-              +
-            </button>
-          </div>
-          <button className="suSearch-btn">搜尋</button>
-        </div>
-      </div>
+    
 
       {/* 地區選擇 Modal */}
       <div
