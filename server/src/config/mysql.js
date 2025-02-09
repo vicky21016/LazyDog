fe427ffebbcf371cff464b7a150c3ed967c55979
Zoe <a033117336@gmail.com>
2025-02-09 14:50:48 +0800

    0209 測試資料庫連接

--- server/src/config/mysql.js
+++ server/src/config/mysql.js
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
 
 export default connectToDatabase;
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DBDB_DATABASE:", process.env.DB_DATABASE);
+// console.log("DB_HOST:", process.env.DB_HOST);
+// console.log("DB_USER:", process.env.DB_USER);
+// console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+// console.log("DBDB_DATABASE:", process.env.DB_DATABASE);
