c6607a2593fdb10671928b35c3f0eb8f57ea6a9c
Feng <qwe5551r@gmail.com>
2025-02-10 17:09:36 +0800

    0210 checktoken

--- server/src/routes/hotelRoutes.js
+++ server/src/routes/hotelRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllHotels } from "../controllers/hotelController.js";
+import { getAllHotels,getById ,createHotel} from "../controllers/hotelController.js";
 
 const router = express.Router();
 
 router.get("/hotels", getAllHotels);
-
+router.get("/hotels/:id", getById);
+router.post("/hotels", createHotel);
 export default router;
