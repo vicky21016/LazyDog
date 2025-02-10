4f7748e4e4f10d6b0c9b12d750350f6ee5d43158
Ajin <asd80207@gmail.com>
2025-02-10 16:03:40 +0800

    pd router暫時

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -4,6 +4,11 @@ import {getAllProducts} from "../controllers/productController.js";
 const router = express.Router();
 
 router.get("/products", getAllProducts);
+// router.get("/products", getAllProducts);
+// router.get("/products", getAllProducts);
+// router.post("/products", getAllProducts);
+// router.put("/products", getAllProducts);
+// router.delete("/products", getAllProducts);
 
 // router.post
 
