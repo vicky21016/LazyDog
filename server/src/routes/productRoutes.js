ac27b1abea6c9ea46e0d5e6c8943e030cca2b47e
Ajin <asd80207@gmail.com>
2025-02-10 20:48:25 +0800

    server router設定

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
-import {getAll} from "../controllers/productController.js";
+import {getAll, getId, getSearch, createNew, connectError} from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.get("/products", getAll);
-// router.get("/products/:id", getAllProducts);
-// router.get("/products/search", getAllProducts);
-// router.post("/products", getAllProducts);
+router.get("/products/search", getSearch); //search要在productID前面
+router.get("/products/:productID", getId);
+router.post("/products", createNew);
 // router.put("/products/:id", getAllProducts);
 // router.delete("/products/:id", getAllProducts);
+router.get("/products/*", connectError);
 
 // router.post
 
--- server/src/services/productService.js
+++ server/src/services/productService.js
@@ -5,17 +5,33 @@ export const getAllProducts = async () => {
     const [products] = await pool.execute("SELECT * FROM yi_product");
     return products;
   } catch (error) {
-    throw new Error(" 無法取得商品列表：" + error.message);
+    throw new Error("無法取得商品列表");
   }
 };
 
-export const getProductId = async (id) => {
+export const getProductId = async productID => {
   try {
-    if(!id) throw new Error("請提供id");
-    const [products] = await pool.execute("SELECT * FROM yi_product",[id]);
+    const [products] = await pool.execute("SELECT * FROM yi_product WHERE productID = ?", [productID]);
     return products;
   } catch (error) {
-    throw new Error(" 無法取得商品：" + error.message);
+    throw new Error(`無法取得編號：${productID}的商品資料`);
   }
 };
 
+export const getSearchKeyword = async keyword => {
+  try {
+    const [products] = await pool.execute("SELECT * FROM yi_product WHERE name LIKE ?", [`%${keyword}%`]);
+    return products;
+  } catch (error) {
+    throw new Error("無法取得相關商品資料：");
+  }
+};
+
+export const createNewItem = async keyword => {
+  try {
+    const [products] = await pool.execute("SELECT * FROM yi_product WHERE name LIKE ?", [`%${keyword}%`]);
+    return products;
+  } catch (error) {
+    throw new Error("無法取得相關商品資料：");
+  }
+};
