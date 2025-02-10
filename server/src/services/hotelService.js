f302e034631a3c93c2e9f045c8d2aa2ebe025d96
Zoe <a033117336@gmail.com>
2025-02-10 16:28:45 +0800

    0210

--- server/src/services/hotelService.js
+++ server/src/services/hotelService.js
@@ -1,31 +1,29 @@
 import pool from "../config/mysql.js";
 
-
-
-export const getHotels = async () => {  
-    try {
-        const [hotels] = await pool.execute("SELECT * FROM hotel");
-        return hotels;
-        
-    } catch (error) {
-        throw new Error(" 無法取得旅館列表：" + error.message);
-    }
+export const getHotels = async () => {
+  try {
+    const [hotels] = await pool.query("SELECT * FROM hotel");
+    return hotels;
+  } catch (error) {
+    throw new Error(" 無法取得旅館列表：" + error.message);
+  }
 };
 
-export const getId = async (id) => {  
-    try {
-        const [hotels] = await pool.query("SELECT * FROM hotel WHERE id = ?", [id]);
-        return hotels;
-    } catch (error) {
-        throw new Error(" 無法取得 ${id} 旅館:;" + error.message);
-    }
+export const getId = async (id) => {
+  try {
+    const [hotels] = await pool.query("SELECT * FROM hotel WHERE id = ?", [id]);
+    return hotels;
+  } catch (error) {
+    throw new Error(" 無法取得 ${id} 旅館:;" + error.message);
+  }
 };
 
-export const createHotels = async () =>{
-    try{
-        const [hotels] = await pool.query (`INSERT INTO  (id, name, , name, mail, head) 
-      VALUES (?, ?, ?, ?, ?, ?)`)
-    }catch(err){
-        throw new Error(" 無法建立旅館：" + error.message);
-    }
-}
+export const createHotels = async () => {
+  try {
+    const [hotels] =
+      await pool.query(`INSERT INTO  (id, name, , name, mail, head) 
+      VALUES (?, ?, ?, ?, ?, ?)`);
+  } catch (err) {
+    throw new Error(" 無法建立旅館：" + error.message);
+  }
+};
