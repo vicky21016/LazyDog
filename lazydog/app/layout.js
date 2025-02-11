cbd66458a2a00e6f472c02321117a6a7ac04923b
Zoe <a033117336@gmail.com>
2025-02-12 00:06:11 +0800

    CLASSNAME

--- lazydog/app/layout.js
+++ lazydog/app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap"
           rel="stylesheet"
