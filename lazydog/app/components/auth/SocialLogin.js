4dc3159be880e4c91fc684d03038e05c60066ecf
Zoe <a033117336@gmail.com>
2025-02-14 17:58:36 +0800

    0214調整轉跳

--- lazydog/app/components/auth/SocialLogin.js
+++ lazydog/app/components/auth/SocialLogin.js
@@ -3,25 +3,19 @@
 import { useAuth } from "@/hooks/use-auth";
 import Link from "next/link";
 export default function SocialLogin() {
-  const { googleLogin, user, logout } = useAuth();
+  const { googleLogin } = useAuth();
 
   return (
     <div className="lumi-social-login">
-      {user ? (
-        <div>
-          <p>歡迎, {user.name}</p>
-          <img src={user.avatar} alt="User Avatar" width="50" />
-          <button onClick={logout}>登出</button>
-        </div>
-      ) : (
-        <button onClick={googleLogin} className="lumi-social-button">
-          <img
-            src="/images/Google.png"
-            alt="google"
-            className="lumi-google-icon"
-          />
-        </button>
-      )}
+      {/* Google 登入按鈕 */}
+      <button onClick={googleLogin} className="lumi-social-button">
+        <img
+          src="/images/Google.png"
+          alt="google"
+          className="lumi-google-icon"
+        />
+      </button>
+
       {/* LINE 登入按鈕 */}
       <Link href="/line/google-login" className="lumi-social-button">
         <img src="/images/line.webp" alt="line" className="lumi-line-icon" />
