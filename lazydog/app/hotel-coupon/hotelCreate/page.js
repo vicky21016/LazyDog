d0f67c7512fd60d3feaf12c54663870316d0b417
Vicky <vicky0909wang@gmail.com>
2025-02-19 15:20:09 +0800

    0219-3

--- lazydog/app/hotel-coupon/hotelCreate/page.js
+++ lazydog/app/hotel-coupon/hotelCreate/page.js
@@ -4,6 +4,7 @@ import styles from "../../../styles/modules/operatorCamera.module.css";
 import hotelStyles from "../../../styles/modules/operatorHotel.module.css";
 import { useRouter } from "next/navigation";
 import { usePhotoUpload } from "@/hooks/usePhotoUpload";
+import My from "../../components/hotel/my"
 import Header from "../../components/layout/header";
 import Menu from "../../components/hotel/menu";
 export default function HotelEditPage(props) {
@@ -50,104 +51,7 @@ export default function HotelEditPage(props) {
         <div className="row">
           {/* 左邊*/}
           {/* <Menu/> */}
-          <div className="col-md-3">
-            <div className="card p-3">
-              <div className="text-center">
-                <div className="position-relative d-inline-block">
-                  <img
-                    ref={avatarRef}
-                    src="/hotel/hotel-images/page-image/Dog2.png"
-                    alt="User Avatar"
-                    className={`rounded-circle ${styles.suAvatarImg}`}
-                  />
-
-                  <div className={styles.dropdownItem}>
-                    <button
-                      className={`btn btn-light ${styles.suCameraIcon}`}
-                      type="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      <img
-                        src="/hotel/hotel-images/page-image/icon-camera.png"
-                        alt="相機"
-                        className={styles.suCameraIconImg}
-                      />
-                    </button>
-                    <ul className={`dropdown-menu ${styles.suDropdownMenu}`}>
-                      <li>
-                        <button
-                          className={`text-danger dropdown-item ${styles.suDropdownItem}`}
-                          onClick={deletePhoto}
-                        >
-                          刪除照片
-                        </button>
-                      </li>
-                      <li>
-                        <label
-                          onClick={uploadPhoto}
-                          className={`dropdown-item ${styles.dropdownItem}`}
-                        >
-                          上傳照片
-                        </label>
-                        <input
-                          type="file"
-                          id="uploadPhoto"
-                          accept="image/*"
-                          className="d-none"
-                          ref={fileInputRef}
-                          onChange={fileChange}
-                        />
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <h5 className="mt-2">陳大方</h5>
-                <p className="text-muted">165846hote@gmail.com</p>
-                <button className="btn btn-outline-success btn-sm">
-                  已認證
-                </button>
-              </div>
-              <hr />
-              <ul className="list-unstyled text-start">
-                <li
-                  className="py-2"
-                  onClick={() => changepage("operatorDetail")}
-                >
-                  <a
-                    className="text-decoration-none text-dark"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <i className="bi bi-person-fill me-2"></i>負責人資訊
-                  </a>
-                </li>
-                <li className="py-2" onClick={() => changepage("hotel")}>
-                  <a
-                    className="text-decoration-none text-dark"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <i className="bi bi-house-heart-fill me-2"></i>旅館資訊
-                  </a>
-                </li>
-                <li className="py-2" onClick={() => changepage("review")}>
-                  <a
-                    className="text-decoration-none text-dark"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <i className="bi bi-card-list me-2"></i>旅館評論
-                  </a>
-                </li>
-                <li className="py-2" onClick={() => changepage("couponList")}>
-                  <a
-                    className="text-decoration-none text-dark"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <i className="bi bi-ticket-perforated me-2"></i>旅館優惠券
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+        <My/>
 
           {/* 右邊 */}
           <div className="col-md-9">
--- lazydog/app/hotel-coupon/hotelEdit/page.js
+++ lazydog/app/hotel-coupon/hotelEdit/page.js
@@ -1,13 +1,16 @@
 "use client";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useContext, useState, useRef } from "react";
 import styles from "../../../styles/modules/operatorCamera.module.css";
 import hotelStyles from "../../../styles/modules/operatorHotel.module.css";
-import { useRouter } from "next/navigation";
 import { usePhotoUpload } from "@/hooks/usePhotoUpload";
+import { useRouter } from "next/navigation";
+import My from "../../components/hotel/my"
 import Header from "../../components/layout/header";
 export default function HotelEditPage(props) {
   const imageUploadRef = useRef(null);
   const [hotelName, setHotelName] = useState("寵物樂園");
+   const { fileInputRef, avatarRef, uploadPhoto, fileChange, deletePhoto } =
+      usePhotoUpload("/images/hotel/hotel-images/page-image/default-avatar.png");
   const [address, setAddress] = useState("台北市中山區 123 號");
   const [phone, setPhone] = useState("02-12345678");
   const [description, setDescription] = useState(
@@ -33,8 +36,7 @@ export default function HotelEditPage(props) {
   };
 
   const router = useRouter();
-  const { fileInputRef, avatarRef, uploadPhoto, fileChange, deletePhoto } =
-    usePhotoUpload("/images/hotel/hotel-images/page-image/default-avatar.png");
+  
   const changepage = (path) => {
     if (path) {
       router.push(`/hotel-coupon/${path}`);
@@ -49,108 +51,10 @@ export default function HotelEditPage(props) {
   return (
     <>
     <Header />
-      <div className="container mt-5">
+      <div className="container my-5">
         <div className="row">
           {/* 左邊*/}
-          <div className="col-md-3">
-            <div className="card p-3">
-              <div className="text-center">
-                <div className="position-relative d-inline-block">
-                  <img
-                    ref={avatarRef}
-                    src="/hotel/hotel-images/page-image/Dog2.png"
-                    alt="User Avatar"
-                    className={`rounded-circle ${styles.suAvatarImg}`}
-                  />
-
-                  <div className={styles.dropdownItem}>
-                    <button
-                      className={`btn btn-light ${styles.suCameraIcon}`}
-                      type="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      <img
-                        src="/hotel/hotel-images/page-image/icon-camera.png"
-                        alt="相機"
-                        className={styles.suCameraIconImg}
-                      />
-                    </button>
-                    <ul className={`dropdown-menu ${styles.suDropdownMenu}`}>
-                      <li>
-                        <button
-                          className={`text-danger dropdown-item ${styles.suDropdownItem}`}
-                          onClick={deletePhoto}
-                        >
-                          刪除照片
-                        </button>
-                      </li>
-                      <li>
-                        <label
-                          onClick={uploadPhoto}
-                          className={`dropdown-item ${styles.dropdownItem}`}
-                        >
-                          上傳照片
-                        </label>
-                        <input
-                          type="file"
-                          id="uploadPhoto"
-                          accept="image/*"
-                          className="d-none"
-                          ref={fileInputRef}
-                          onChange={fileChange}
-                        />
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <h5 className="mt-2">陳大方</h5>
-                <p className="text-muted">165846hote@gmail.com</p>
-                <button className="btn btn-outline-success btn-sm">
-                  已認證
-                </button>
-              </div>
-
-              <hr />
-              <ul className="list-unstyled text-start">
-                <li
-                  className="py-2"
-                  onClick={() => changepage("operatorDetail")}
-                >
-                  <a
-                    className="text-decoration-none text-dark"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <i className="bi bi-person-fill me-2"></i>負責人資訊
-                  </a>
-                </li>
-                <li className="py-2" onClick={() => changepage("hotel")}>
-                  <a
-                    className="text-decoration-none text-dark"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <i className="bi bi-house-heart-fill me-2"></i>旅館資訊
-                  </a>
-                </li>
-                <li className="py-2" onClick={() => changepage("review")}>
-                  <a
-                    className="text-decoration-none text-dark"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <i className="bi bi-card-list me-2"></i>旅館評論
-                  </a>
-                </li>
-                <li className="py-2" onClick={() => changepage("couponList")}>
-                  <a
-                    className="text-decoration-none text-dark"
-                    style={{ cursor: "pointer" }}
-                  >
-                    <i className="bi bi-ticket-perforated me-2"></i>旅館優惠券
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+         <My/>
 
           {/* 右邊 */}
           <div className="col-md-9  mx-auto">
