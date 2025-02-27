d1be0d0417217c48bed242ba36d404a30d34c3d8
Zoe <a033117336@gmail.com>
2025-02-27 10:05:38 +0800

    01

--- lazydog/app/components/hotel/roomSelection.js
+++ lazydog/app/components/hotel/roomSelection.js
@@ -16,7 +16,6 @@ const RoomSelection = ({ hotelId }) => {
 
     const fetchRooms = async () => {
       try {
-        console.log(`🔍 請求 API: /api/hotel_room_types/${hotelId}`);
 
         let roomTypes = await getHotelRoomById(hotelId);
         if (!Array.isArray(roomTypes)) {
@@ -60,7 +59,10 @@ const RoomSelection = ({ hotelId }) => {
       {loading ? (
         <p className="text-center">載入中...</p>
       ) : rooms.length > 0 ? (
-        <div className="row mt-4">
+        <div
+          className={`row mt-4 ${
+            rooms.length == 1 ? "justify-content-center" : ""}`}
+        >
           {rooms.map((room, index) => (
             <div key={index} className="col-lg-4 col-md-6 col-sm-12 mb-4">
               <div className={`card ${hotelStyles.suRoomCard}`}>
