a267795b70d6697a8d51c6831d01c23e4eaf66c1
Feng <qwe5551r@gmail.com>
2025-03-03 22:33:11 +0800

    0303

--- lazydog/app/orders/_components/product.js
+++ lazydog/app/orders/_components/product.js
@@ -3,16 +3,20 @@ import Link from 'next/link'; // 如果你使用的是 Next.js 的 Link 元件
 import couponStyles from './userCoupon.module.css';
 
 const Product = ({ orders }) => {
-  //過濾出商品類型的訂單
-  const productOrders = orders.filter(order => order.orderID);
+    //過濾出商品類型的訂單
+    const productOrders = orders.filter(order => order.orderID);
 
     return (
         <>
             {productOrders.map((order) => (
-                <div key={order.orderID} className={`mt-2 ${couponStyles.suCouponCard}`}>
+
+
+                < div key={order.orderID} className={`mt-2 ${couponStyles.suCouponCard}`}>
                     <Link href="">
                         <img
-                            src="http://localhost:5000/api/articles/2df54e20-d6c0-11ee-beff-f3978ced.jpg"
+                            src={`/product/img/${(
+                                order.name
+                            )}`}
                             style={{ maxHeight: '100px' }}
                             alt={order.orderID}
                         />
@@ -25,7 +29,7 @@ const Product = ({ orders }) => {
 
                     </div>
                     <span className={couponStyles.suPrice}>NT{order.final_amount}</span>
-                </div>
+                </div >
             ))}
         </>
     );
--- lazydog/hooks/use-cart.js
+++ lazydog/hooks/use-cart.js
@@ -32,22 +32,23 @@ export function CartProvider({ children }) {
   };
 
   // 課程增減邏輯
-  const onAddCourse = (course, amount = 1, session, time) => {
+  const onAddCourse = (course, session, time) => {
+ 
     const foundIndex = courseItems.findIndex((v) => v.id === course.id);
     if (foundIndex !== -1) {
       const nextItems = courseItems.map((v) =>
-        v.id === course.id ? { ...v, count: v.count + amount } : v
+        v.id === course.id ? { ...v, count: v.count + 1 } : v
       );
       setCourseItems(nextItems);
     } else {
-      const newItem = { ...course, count: amount };
+      const newItem = { ...course, count: 1 };
       setCourseItems([newItem, ...courseItems]);
     }
   };
 
   // 增加旅館的邏輯
   const onAddHotel = (hotel, amount = 1) => {
-    console.log({ hotel });
+    // console.log({ hotel });
 
     const foundIndex = hotelItems.findIndex((v) => v.id === hotel.id);
     if (foundIndex !== -1) {
