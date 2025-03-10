4214dc15a0495b197d6acadce1679d9067b4185e
Feng <qwe5551r@gmail.com>
2025-03-10 13:38:32 +0800

    0310 usecontext 把它弄壞看看

--- lazydog/app/user/orders/_components/hotel.js
+++ lazydog/app/user/orders/_components/hotel.js
@@ -34,7 +34,7 @@ const Hotel = ({ hotelOrders }) => {
               </p>
             </div>
             <span className={couponStyles.suPrice}>
-              NT {order.final_amount.split(".")}
+              NT {order.final_amount.split(".")[0]}
             </span>
           </div>
         );
