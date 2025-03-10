df3c4c462248e81640ee1f02b5c3b4a2b44ed7f7
tsai <cherry60825@gmail.com>
2025-03-11 05:41:43 +0800

    0311 02輪播一半(先這樣)

--- lazydog/app/course/_components/list/other-course-card.js
+++ lazydog/app/course/_components/list/other-course-card.js
@@ -1,12 +1,15 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import styles from "../courseList.module.css";
 import Link from "next/link";
-// import Slider from "react-slick";
+import { motion } from "framer-motion";
+import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
+import styles from "../courseList.module.css";
 
 export default function OtherCourseCard() {
-  const [latest, setLatest] = useState(null);
+  const [latest, setLatest] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const itemsPerPage = 4;
 
   useEffect(() => {
     fetch(`http://localhost:5000/api/course/`)
@@ -20,6 +23,14 @@ export default function OtherCourseCard() {
   }, []);
   // console.log(latest);
 
+  const nextSlide = () => {
+    setCurrentIndex((prev) => (prev + 1) % latest.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prev) => (prev - 1 + latest.length) % latest.length);
+  };
+
   return (
     <>
       <div className={styles.similarCourse}>
@@ -27,20 +38,60 @@ export default function OtherCourseCard() {
         <div className={styles.sBars}>
           <div className={styles.sbar} />
           <div className={styles.btns}>
-            <img
+            <button
+              type="button"
               className={styles.arrowLeft}
-              src="/course/img/arrow-left.png"
-              alt={`往左箭頭`}
-            />
-            <img
+              onClick={prevSlide}
+            >
+              <FaArrowLeftLong className="fs-5" />
+              {/* <img src="/course/img/arrow-left.png" alt={`往左箭頭`} /> */}
+            </button>
+            <button
+              type="button"
               className={styles.arrowRight}
-              src="/course/img/arrow-right.png"
-              alt={`往右箭頭`}
-            />
+              onClick={nextSlide}
+            >
+              <FaArrowRightLong className="fs-5" />
+              {/* <img src="/course/img/arrow-right.png" alt={`往右箭頭`} /> */}
+            </button>
           </div>
         </div>
-        <div className={`row gy-5 ${styles.sCards}`}>
-          {latest?.map((la) => (
+        {/* <div className={`row gy-5 ${styles.sCards}`}> */}
+        <motion.div
+          className={`row gy-5 ${styles.sCards}`}
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0 }}
+          transition={{ all: 0.1 }}
+        >
+          {latest
+            ?.slice(currentIndex, currentIndex + itemsPerPage)
+            .map((la) => (
+              <motion.div
+                key={la.courseId}
+                layout
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+                transition={{ all: 0.1 }}
+                className={`col-6 col-lg-3 m-0 ${styles.sCard}`}
+              >
+                <Link
+                  href={`/course/${la.courseId}`}
+                  className={styles.linkStyle}
+                >
+                  <img
+                    className={styles.cardImg}
+                    src={`/course/img/${la.img_url}`}
+                    alt={la.courseName}
+                  />
+                  <h5 className={styles.cardName}>{la.courseName}</h5>
+                </Link>
+              </motion.div>
+            ))}
+        </motion.div>
+
+        {/* {latest?.map((la) => (
             <Link
               className={`col-6 col-lg-3 m-0 ${styles.sCard}`}
               key={la.courseId}
@@ -53,8 +104,8 @@ export default function OtherCourseCard() {
               />
               <h5 className={styles.cardName}>{la.courseName}</h5>
             </Link>
-          ))}
-        </div>
+          ))} */}
+        {/* </div> */}
       </div>
     </>
   );
