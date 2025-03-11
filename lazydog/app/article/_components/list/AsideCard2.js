6ae0e0e0f9d7a146ae5f2df0fd173b0c414b2911
Wang-Hsiang <guitarwang0507@gmail.com>
2025-03-11 14:50:53 +0800

    article小修

--- lazydog/app/article/_components/list/AsideCard2.js
+++ lazydog/app/article/_components/list/AsideCard2.js
@@ -7,28 +7,32 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 export default function AsideCard({ id, title, cover_image, created_at }) {
     return (
         <>
-           
-           <Link
+
+            <Link
                 href={{ pathname: `/article/detail/${id}`, query: { list: "true" } }}
                 // className={styles.asideRead}
                 className={`${styles.asideRead2} row mt-2`}
             >
-                <div className="col-3">
-                    <div
-                    className="ratio ratio-1x1 w-100"
-                    >
-                        <img
-                            // className="w-100"
-                            src={cover_image}
-                            alt=""
-                            style={{ objectFit: 'cover' }}
-                        />
-                    </div>
-                </div>
                 <div
-                    className="col-9">
-                    <div className={styles.title}>{title}</div>
-                    <p style={{ color: '#9F9F9F' }}>{created_at}</p>
+                    style={{ display: 'flex' }}
+                >
+                    <div className="col-3">
+                        <div
+                            className="ratio ratio-1x1 w-100"
+                        >
+                            <img
+                                // className="w-100"
+                                src={cover_image}
+                                alt=""
+                                style={{ objectFit: 'cover' }}
+                            />
+                        </div>
+                    </div>
+                    <div
+                        className="col-9">
+                        <div className={styles.title2}>{title}</div>
+                        <p style={{ color: '#9F9F9F' }}>{created_at}</p>
+                    </div>
                 </div>
             </Link>
         </>
