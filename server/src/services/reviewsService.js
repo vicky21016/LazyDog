7a2f3828ed14053f72748674fdaad019949efd92
vicky <vicky0909wang@gmail.com>
2025-03-13 15:11:36 +0800

    313

--- server/src/services/reviewsService.js
+++ server/src/services/reviewsService.js
@@ -37,16 +37,19 @@ export const replycourseReviews = async (review_id, operator_id, reply) => {
     throw new Error("無法回覆評論：" + error.message);
   }
 };
-export const getcourseReviews = async (course_id) => {
+export const getcourseReviews = async (teacher_id) => {
   try {
     // 取得評論（不包含圖片）
     const [reviews] = await pool.query(
       `SELECT cr.*, u.name AS user_name
-       FROM course_reviews cr
-       JOIN users u ON cr.user_id = u.id
-       WHERE cr.course_id = ? AND cr.is_deleted = 0 
-       ORDER BY cr.created_at DESC`,
-      [course_id]
+             FROM course_reviews cr
+             JOIN users u ON cr.user_id = u.id
+             JOIN course_session cs ON cr.course_id = cs.course_id
+             WHERE cs.teacher_id = ? AND cr.is_deleted = 0
+             GROUP BY cr.id
+             ORDER BY cr.created_at DESC;
+`,
+      [teacher_id]
     );
     if (reviews.length == 0) {
       return { success: true, message: "此飯店沒有評論", data: [] };
